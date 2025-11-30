a1b7e89aaee89b45a420bca8a4fb5d1e5dc6acad
bhalu-git169 <bhalendushrivastava90@gmail.com>
2025-11-30 14:46:59 +0530

    Fix routing for GitHub Pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // add this
 import './index.css';
 import App from './App';
-import './i18n';        // <-- add this line
+import './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/Hospital-management-app---Quickqueue/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+
